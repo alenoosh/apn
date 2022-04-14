57f7938409a4fce2a604c08a419055d18c0a7590
alenoosh <hope2end@gmail.com>
2022-04-14 17:09:18 +0430

    Token AuthProvider is replaced with Certificate AuthProvider.

--- src/ApnServiceProvider.php
+++ src/ApnServiceProvider.php
@@ -4,7 +4,7 @@ namespace NotificationChannels\Apn;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
-use Pushok\AuthProvider\Token;
+use Pushok\AuthProvider\Certificate;
 use Pushok\Client;
 
 class ApnServiceProvider extends ServiceProvider
@@ -16,14 +16,14 @@ class ApnServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(Token::class, function ($app) {
+        $this->app->bind(Certificate::class, function ($app) {
             $options = Arr::except($app['config']['broadcasting.connections.apn'], 'production');
 
-            return Token::create($options);
+            return Certificate::create($options);
         });
 
         $this->app->bind(Client::class, function ($app) {
-            return new Client($app->make(Token::class), $app['config']['broadcasting.connections.apn.production']);
+            return new Client($app->make(Certificate::class), $app['config']['broadcasting.connections.apn.production']);
         });
     }
 }
